c5b088e9b76415989aa819fba42a983424d0cf00
Tonho <162700589+AntonioCavalheiro@users.noreply.github.com>
2024-06-25 10:56:32 -0300

    Add files via upload

---
+++ pw/applikes/src/App.jsx
@@ -0,0 +1,26 @@
+import { Post } from "./Post.1"
+
+export function App() {
+  return (
+    <div>
+      <Post
+      author="Machado de Assis"
+      coment="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis voluptatibus velit, 
+      quae ullam minima, voluptas officiis enim qui officia ipsam labore alias iste, explicabo asperiores 
+      repudiandae dolorum necessitatibus incidunt aperiam."/>
+
+      <Post
+      author="Machado de Assis"
+      coment="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis voluptatibus velit, 
+      quae ullam minima, voluptas officiis enim qui officia ipsam labore alias iste, explicabo asperiores 
+      repudiandae dolorum necessitatibus incidunt aperiam."/>
+
+      <Post
+      author="Machado de Assis"
+      coment="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis voluptatibus velit, 
+      quae ullam minima, voluptas officiis enim qui officia ipsam labore alias iste, explicabo asperiores 
+      repudiandae dolorum necessitatibus incidunt aperiam."/>
+
+    </div>
+  )
+}
---
+++ pw/receitasapp/src/Receitas.jsx
@@ -0,0 +1,9 @@
+export function Receitas(props) {
+    return (
+        <div>
+            <img src="" alt="" />
+            <strong>{props.nomerec}</strong>
+            <p></p>
+        </div>
+    );
+}
