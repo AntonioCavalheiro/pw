533086c12757c8ba09a3a07f0531cb538a97393d
Tonho <162700589+AntonioCavalheiro@users.noreply.github.com>
2024-10-22 10:54:39 -0300

    Add files via upload

---
+++ to do/src/App.jsx
@@ -0,0 +1,14 @@
+import { Text } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Text>
+        oi
+      </Text>
+    </div>
+  )
+}
+
+export default App
---
+++ to do/src/Topo.jsx
@@ -0,0 +1,12 @@
+import { Text } from 'react'
+import './Topo.css'
+
+function Topo() {
+  return (
+    <div>
+      <Text></Text>
+    </div>
+  )
+}
+
+export default Topo
